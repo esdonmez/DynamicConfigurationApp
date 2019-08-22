3825fc5ccbd190c0de36651b2d1b5dea41187844
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2019-08-22 18:10:56 +0300

    namespace is fixed

--- Web_DynamicConfiguration/Startup.cs
+++ Web_DynamicConfiguration/Startup.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DynamicConfigurationManager.Infrastructure;
+using Lib_DynamicConfiguration.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
